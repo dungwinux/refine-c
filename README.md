b0afa2f26e7b4b4a5bfbc3f9f769fad734df2848
Dung Winux <ntddebugger@gmail.com>
2018-07-08 16:39:18 +0700

    Init commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Refine
+
+Got tired of multiple preprocessor in code ? Feel lost ? Refine will help you through that.
+
+## Features
+
+- Replace `#define`, `#if`, ...
+
+- Put all of dependencies in working directory into one file (i.e `#include "myLib.h"`)
+
+- Summary: Running GCC prepocessor with minimal changes to source code
+
+![Refine in Action](.\image\action.gif)
+
+## Requirements
+
+No requirement here, just install and you're done.
+
+## Extension Settings
+
+- `refine-c.enableLanguages`: A list of languages IDs to enable this extension on (any other language except C, C++, Objective-C, Objective-C++ will be automatically rejected)
+- `refine-c.refineLanguage`: Specify the language for `gcc`. If 'none' specified, `gcc` will determine and 'refine' your code based on file type.
+  > You need to install the proper GCC front-end for the language you choose.
+
+## Known Issues
+
+- Changing "refine-c.enableLanguages" can make Refine failed to run
+
+## Release Notes
+
+### 0.0.1
+
+Initial release
---
+++ extension.js
@@ -0,0 +1,84 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+const cp = require("child_process");
+const fs = require("fs");
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+function activate(context) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "refine-c" is now active!');
+
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with  registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "refine-c.refine",
+    function() {
+      // The code you place here will be executed every time your command is executed
+      const currentWorkingFile = vscode.window.activeTextEditor.document;
+      const enableLanguage = vscode.workspace
+        .getConfiguration("refine-c")
+        .get("enableLanguages");
+      // console.log(enableLanguage);
+
+      // Check refineLanguage
+      let refineLang = vscode.workspace
+        .getConfiguration("refine-c")
+        .get("refineLanguage");
+      const supportLang = [
+        "none",
+        "c",
+        "c-header",
+        "c++",
+        "c++-header",
+        "objective-c",
+        "objective-c-header",
+        "objective-c++",
+        "objective-c++-header"
+      ];
+      if (supportLang.indexOf(refineLang) === -1) refineLang = "none";
+      // Check for documentLanguage
+      if (
+        currentWorkingFile.languageId === "c" ||
+        currentWorkingFile.languageId === "cpp" ||
+        currentWorkingFile.languageId === "objective-c" ||
+        currentWorkingFile.languageId === "objective-cpp" ||
+        enableLanguage.indexOf(currentWorkingFile.languageId) !== -1
+      ) {
+        if (!currentWorkingFile) {
+          vscode.window.showInformationMessage("No file was chosen!");
+          return;
+        }
+        if (currentWorkingFile.isUntitled || currentWorkingFile.isDirty) {
+          vscode.window.showInformationMessage("Your file was not saved!");
+          return;
+        }
+        const fileName = currentWorkingFile.fileName;
+
+        // Executing
+        const execLine =
+          "gcc -E -CC -P -undef -dI -nostdinc -x " +
+          refineLang +
+          " " +
+          fileName;
+        cp.exec(execLine, (err, stdout, stderr) => {
+          if (stderr) vscode.window.showInformationMessage("Problem: ", stderr);
+          fs.writeFile(fileName, stdout);
+        });
+
+        // Display a message box to the user
+        vscode.window.showInformationMessage("Your code has been refined!");
+      } else vscode.window.showInformationMessage("Language is not supported!");
+    }
+  );
+
+  context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+exports.deactivate = deactivate;
